2abaafc8549a426682082be68cbb7776131a8dea
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-10 14:12:07 -0300

    gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
-import { CrudComponent } from './crud/crud.component';
+import {CrudComponent} from './crud/crud.component';
 
 @NgModule({
   declarations: [
